aa0bdc459cf95a636fbb08d4ec599fa8490f794c
cruor99 <kliknes@gmail.com>
2016-02-26 01:43:18 +0100

    Closes #13

--- app/main.py
+++ app/main.py
@@ -327,6 +327,14 @@ class SearchPopup(Popup):
         search_store.put("searchstring", searchphrase=searchquery)
         self.dismiss()
 
+    def open_filters(self):
+        fpop = FilterPopup()
+        fpop.bind(on_dismiss=self.set_filters)
+        fpop.open()
+
+    def set_filters(self, instance):
+        App.get_running_app().root.set_filters(instance)
+
 
 class SadpandaRoot(BoxLayout):
 
